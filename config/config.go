03b3c0ccbeadcfc8caadd5a0aa822519c9423b4c
YuheiNakasaka <yuhei.nakasaka@gmail.com>
2018-04-14 21:49:42 +0900

    get config path with environment variable

--- config/config.go
+++ config/config.go
@@ -15,25 +15,28 @@ type Config struct {
 	List *viper.Viper
 }
 
-// Init initializes struct
+// Init initializes struct.
+// Default path is $GOPATH/src/github.com/YuheiNakasaka/radiorec/config/
 func (c *Config) Init() error {
-	b, err := exec.Command("go", "env", "GOPATH").CombinedOutput()
-	if err != nil {
-		return fmt.Errorf("Failed to find path to database: %v", err)
-	}
+	configDir := os.Getenv("CONFIG_PATH")
+	if configDir == "" {
+		b, err := exec.Command("go", "env", "GOPATH").CombinedOutput()
+		if err != nil {
+			return fmt.Errorf("Failed to get GOPATH: %v", err)
+		}
 
-	configDir := ""
-	for _, p := range filepath.SplitList(strings.TrimSpace(string(b))) {
-		p = filepath.Join(p, filepath.FromSlash("/src/github.com/YuheiNakasaka/radiorec/config/"))
-		if _, err = os.Stat(p); err == nil {
-			configDir = p
-			break
+		for _, p := range filepath.SplitList(strings.TrimSpace(string(b))) {
+			p = filepath.Join(p, filepath.FromSlash("/src/github.com/YuheiNakasaka/radiorec/config/"))
+			if _, err = os.Stat(p); err == nil {
+				configDir = p
+				break
+			}
 		}
 	}
 
 	viper.SetConfigName("config")
 	viper.AddConfigPath(configDir)
-	err = viper.ReadInConfig()
+	err := viper.ReadInConfig()
 	if err != nil {
 		return fmt.Errorf("Fatal error config file: %v", err)
 	}
